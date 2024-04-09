dd12e7c9f997caceb22efe9ec07d081448e20218
BanjoInertia <nozembro@gmail.com>
2024-04-09 15:14:36 -0300

    ...

--- src/components/HomePage/HomePageCards/HomePageCards.jsx
+++ src/components/HomePage/HomePageCards/HomePageCards.jsx
@@ -4,13 +4,16 @@ import img from "../../../assets/blitz.png"
 
 export const HomePageCards = () => {
     return (
-        <section className={classes.card}>
-            <img src={img} />
-            <div>
-                <h1>XX99 MARK II HEADPHONES</h1>
-                <p>Experience natural, lifelike audio and excepitional build quality made for the passionate music enthusiast</p>
-                <a><button>SEE PRODUCT</button></a>
+        <>
+            <h1 className={classes.card_titulo}>NOVIDADE!</h1>
+            <div className={classes.card}>
+                <img src={img} />
+                <div>
+                    <h1>XX99 MARK II HEADPHONES</h1>
+                    <p>Experience natural, lifelike audio and excepitional build quality made for the passionate music enthusiast</p>
+                    <a><button>SEE PRODUCT</button></a>
+                </div>
             </div>
-        </section>
+        </>
     )
 }
