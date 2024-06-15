6ecefc80b057d4afa2a6e28fa28fd67a50625b1c
BanjoInertia <nozembro@gmail.com>
2024-06-15 14:01:37 -0300

    tentando arrumar

--- src/pages/routes.jsx
+++ src/pages/routes.jsx
@@ -1,11 +1,11 @@
-import { BrowserRouter, Route, Routes} from 'react-router-dom'
-import { HomePage } from './homePage'
-import { ProductListPage } from './productListPage'
-import { ProductDetailsPage } from './productDetailsPage'
-import { PageHeader } from '../components/PageHeader/PageHeader'
-import { PageFooter } from '../components/PageFooter/PageFooter'
-import { ShoppingCartProvider } from '../context/ShoppingCartContext'
-import { AboutPage } from './aboutPage'
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import { HomePage } from './homePage';
+import { ProductListPage } from './productListPage';
+import { ProductDetailsPage } from './productDetailsPage';
+import { AboutPage } from './aboutPage';
+import { PageHeader } from '../components/PageHeader/PageHeader';
+import { PageFooter } from '../components/PageFooter/PageFooter';
+import { ShoppingCartProvider } from '../context/ShoppingCartContext';
 
 export const AppRoutes = () => {
     return (
@@ -13,13 +13,15 @@ export const AppRoutes = () => {
             <BrowserRouter>
                 <PageHeader />
                 <Routes>
-                    <Route path='/' element={<HomePage />}/>
-                    <Route path='/products' element={<ProductListPage />}/>
-                    <Route path='/product/:id' element={<ProductDetailsPage />}/>
-                    <Route path='/about' element={<AboutPage />}/>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/products" element={<ProductListPage />} />
+                    <Route path="/product/:id" element={<ProductDetailsPage />} />
+                    <Route path="/about" element={<AboutPage />} />
+                    {/* 404 Route */}
+                    <Route path="*" element={<Navigate to="/" />} />
                 </Routes>
                 <PageFooter />
             </BrowserRouter>
         </ShoppingCartProvider>
-    )
-}
+    );
+};
